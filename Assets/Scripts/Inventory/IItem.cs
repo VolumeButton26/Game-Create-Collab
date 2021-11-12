0289fdbaab81b540ccca4e876feb884d20b00e2f
Alizer <saylent1@gmail.com>
2021-11-12 22:30:48 +0800

    ✍️ Interface of Items

--- Assets/Scripts/Inventory/IItem.cs
+++ Assets/Scripts/Inventory/IItem.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IItem : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public interface IItem {
+    string ItemName { get; set; }
+    string ItemDescription { get; set; }
 }
