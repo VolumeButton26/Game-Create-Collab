a7f5218a97d59c4acd584224d09f2338e033a9a6
Alizer <saylent1@gmail.com>
2021-11-12 23:19:07 +0800

    🔨 Placeholders for Seashell Necklace.

--- Assets/Scripts/Inventory/Items/Seashell Necklace.cs
+++ Assets/Scripts/Inventory/Items/Seashell Necklace.cs
@@ -8,5 +8,27 @@ namespace Inventory.Items {
 
     // this is one item right? not sure if this is a type of item maybe we should interface this too
     public class SeashellNecklace : IItem {
+
+        private string _name = "Seashell Necklace";
+
+        private string _description = "This thing does...things.";
+
+        public string ItemName {
+            get {
+                return this._name;
+            }
+            set {
+                this._name = value;
+            }
+        }
+
+        public string ItemDescription {
+            get {
+                return this._description;
+            }
+            set {
+                this._description = value;
+            }
+        }
     }
 }
